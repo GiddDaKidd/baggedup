fa339c41779f67cef239506f47e775ca412c7a05
Gideon <mwaniki59@gmail.com>
2017-09-13 14:24:47 -0500

    channging controller to publish app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :reject_methods
-
-def reject_methods
-  if ['TRACE','PATCH'].include?(request.method)
-    #raise ActionController::RoutingError.new('Not Found')
-    raise ActionController::MethodNotAllowed.new('Method not allowed')  #405
-    # or whatever you want to do (redirect, error message, ...)
-  end
-end
-
 end
