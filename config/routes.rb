3bee3ec05170d18969b065b3fd00a1c425bba9b4
Gideon <mwaniki59@gmail.com>
2017-09-13 13:49:38 -0500

    adding images and styles also configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'home', to: 'welcome#index'
+  get 'category', to: 'category#category'
+  get 'cart', to: 'cart#cart'
+  get 'blog', to: 'blog#blog'
+  get 'blog_post', to: 'blog_post#blog_post'
+  get 'contact', to: 'contact#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
